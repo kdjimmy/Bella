16095f07ea0d24a4d8549ee44fbcefc593f34b4d
shuxin-bst <shuxin.wang@bluestacks>
2026-01-03 18:10:32 +0800

    2026-1-3

--- headers/FrameGraph.h
+++ headers/FrameGraph.h
@@ -1,6 +1,26 @@
-#include <vulkan/vulkan.h>
-#include <vector>
-#include <mutex>
-#include <functional>
-#include <iostream>
-#include <stdio.h>
+#include "RenderPass.h"
+#include <memory>
+namespace FrameGraph
+{
+    class FrameGraph
+    {
+        public:
+            VkDevice device = VK_NULL_HANDLE;
+            FrameGraph(const VkDevice& _device):device(_device)
+            {
+                printf("createing frame graph device = 0x%p", &device); 
+            }
+
+            inline RenderPass& addPass(const RenderPass& pass)
+            {
+
+            }
+        private:
+                std::vector<std::shared_ptr<RenderPass>> passes;
+                std::vector<std::shared_ptr<Resource::RenderResource>> resources;
+                std::unordered_map<std::string, unsigned> pass_to_index;
+                std::unordered_map<std::string, unsigned> resource_to_index;
+                std::vector<std::unordered_set<unsigned>> pass_dependencies;
+                std::vector<std::unordered_set<unsigned>> pass_merge_dependencies;
+                std::string backbuffer_source;
+    }
---
+++ headers/RenderPass.h
@@ -0,0 +1,6 @@
+#include "Resources.h"
+
+class RenderPass
+{
+
+};
